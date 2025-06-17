627df65d2f8d69a0ed28ba77de6f8acd021ce04d
Rekha V <153058445+rekzz555@users.noreply.github.com>
2025-06-18 01:21:31 +0530

    Add files via upload

---
+++ LoginApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package testapp.loginapp;
+
+/**
+ *
+ * @author User
+ */
+public class LoginApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
